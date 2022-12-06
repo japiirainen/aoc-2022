91119a602827b4d4cc7bfdde9f11b6adaa060ab2
Joona Piirainen <joona.piirainen@gmail.com>
2022-12-06 09:15:23 +0200

    day 06 in lean4

--- src/06.lean
+++ src/06.lean
@@ -1,19 +1,8 @@
 import Std
 
-def sol : IO Unit := do
-  let ip ← IO.FS.readFile "src/06"
-  IO.println ip
+def solution (s : String) (n : Nat) : Nat :=
+  (· + n) $ s.toList.tails.findIdx
+    ((· = n) ∘ (·.toSSet.size) ∘ (·.map (·.toNat)) ∘ (·.take n))
 
-def ex1 := "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
-
-def startOfPacket (s: String) : Nat :=
-  let rec go (acc : Nat) (s : String) : Nat :=
-    match s.toList with
-    | a :: b :: c :: d :: rest => 
-      if a == b && b == c && c == d then acc + 4
-      else go (acc + 4) rest.toString
-    | _ => acc
-    -- | _ => go (acc + 1) (s.drop 1)
-  go 0 s
-
--- #eval startOfPacket ex1
+#eval do IO.println (solution (← IO.FS.readFile "src/06") 4)
+#eval do IO.println (solution (← IO.FS.readFile "src/06") 14)
