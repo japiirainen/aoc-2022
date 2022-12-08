ed2834cb3ec083f54ddd549b4daf548aa5ad2103
Joona Piirainen <joona.piirainen@gmail.com>
2022-12-08 20:39:13 +0200

    reformat hs sources

--- hs/08.hs
+++ hs/08.hs
@@ -5,29 +5,29 @@ import AOC.List.Extended (countBy)
 import AOC.Main (simpleMain)
 import Data.Array.Unboxed (IArray (..), Ix (inRange, range), UArray, listArray, (!))
 
-main :: IO ()
-main = simpleMain $ \input ->
+main ∷ IO ()
+main = simpleMain $ \input →
   let arr = toArray (lines input)
    in ( countBy (isVisible arr) (range (bounds arr))
       , maximum (map (scenicScore arr) (range (bounds arr)))
       )
 
-isVisible :: UArray Coord Char -> Coord -> Bool
+isVisible ∷ UArray Coord Char → Coord → Bool
 isVisible a c = any (clearView . toEdge a c) [above, below, left, right]
   where
     clearView [] = error "unreachable"
     clearView (x : xs) = all (< x) xs
 
-toEdge :: UArray Coord Char -> Coord -> (Coord -> Coord) -> [Char]
-toEdge a c d = [a ! i | i <- takeWhile (inRange (bounds a)) (iterate d c)]
+toEdge ∷ UArray Coord Char → Coord → (Coord → Coord) → [Char]
+toEdge a c d = [a ! i | i ← takeWhile (inRange (bounds a)) (iterate d c)]
 
-scenicScore :: UArray Coord Char -> Coord -> Int
+scenicScore ∷ UArray Coord Char → Coord → Int
 scenicScore a c = product (map (treesSeen . toEdge a c) [above, below, left, right])
   where
     treesSeen [] = error "unreachable"
     treesSeen (x : xs) = case break (>= x) xs of
-      (d, []) -> length d
-      (d, _ : _) -> length d + 1
+      (d, []) → length d
+      (d, _ : _) → length d + 1
 
-toArray :: [String] -> UArray Coord Char
+toArray ∷ [String] → UArray Coord Char
 toArray ls = listArray (C 0 0, C (length ls - 1) (length (head ls) - 1)) (concat ls)
