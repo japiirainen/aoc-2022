03ef94a4ba72c974dc3eafa5967d0552846cbf8e
Joona Piirainen <joona.piirainen@gmail.com>
2022-12-12 18:30:23 +0200

    remove js and lean stuff

--- lean/06.lean
+++
@@ -1,8 +0,0 @@
-import Std
-
-def solution (s : String) (n : Nat) : Nat :=
-  (· + n) $ s.toList.tails.findIdx
-    ((· = n) ∘ (·.toSSet.size) ∘ (·.map (·.toNat)) ∘ (·.take n))
-
-#eval do IO.println (solution (← IO.FS.readFile "input/06") 4)
-#eval do IO.println (solution (← IO.FS.readFile "input/06") 14)
