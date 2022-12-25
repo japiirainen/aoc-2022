88a7b108f26cc0ad21e5a8573b015ad71d12109e
Joona Piirainen <joona.piirainen@gmail.com>
2022-12-25 11:16:21 +0200

    clean up repo

--- 2022/01P1.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-input = [[int(line) for line in g.splitlines()] for g in open('input/01').read().split('\n\n')]
-max_group = max([sum(g) for g in input])
-assert 72478 == max_group
-print(max_group)
